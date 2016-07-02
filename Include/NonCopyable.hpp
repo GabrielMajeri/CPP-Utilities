178495c0b678c547e3ce0f9e6f841842da30d47d
Gabriel M <gabriel.majeri6@gmail.com>
2016-07-02 16:22:09 +0300

    Remove namespace from original source

--- Include/NonCopyable.hpp
+++ Include/NonCopyable.hpp
@@ -1,19 +1,16 @@
 #pragma once
 
-namespace VFS
+/// This class prevents its inheritors from being copied.
+class NonCopyable
 {
-	/// This class prevents its inheritors from being copied.
-	class NonCopyable
-	{
-	protected:
-		NonCopyable() noexcept = default;
-		~NonCopyable() = default;
+protected:
+	NonCopyable() noexcept = default;
+	~NonCopyable() = default;
 
-		NonCopyable(NonCopyable&&) noexcept = default;
-		NonCopyable& operator=(NonCopyable&&) noexcept = default;
+	NonCopyable(NonCopyable&&) noexcept = default;
+	NonCopyable& operator=(NonCopyable&&) noexcept = default;
 
-	private:
-		NonCopyable(const NonCopyable&) noexcept = delete;
-		NonCopyable& operator=(const NonCopyable&) noexcept = delete;
-	};
-}
+private:
+	NonCopyable(const NonCopyable&) noexcept = delete;
+	NonCopyable& operator=(const NonCopyable&) noexcept = delete;
+};
